b2d6ff612521a6f3aa6fa472311bf05dd0800998
Freddy <122462360+FreddZas@users.noreply.github.com>
2023-08-08 23:36:23 -0500

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import HomePage from './pages/Homepage'
+import HomePage from './pages/HomePage'
 import PokedexPage from './pages/PokedexPage'
 import PokeIdPage from './pages/PokeIdPage'
 import Poke404 from './pages/Poke404'
